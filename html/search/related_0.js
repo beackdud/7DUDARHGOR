8567021b192223b2cc93e7cbe2051e8076c45634
Dudchenko Arhipov <BeackDud1241@gmail.com>
2024-10-29 00:16:49 +0300

    new version with picthure

---
+++ html/search/related_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['operator_2a_0',['operator*',['../class_complex.html#a2518193c907c8db8fe5920468f8d6771',1,'Complex']]],
+  ['operator_2b_1',['operator+',['../class_complex.html#a4b1807e2221e04ba8661276644a7eacb',1,'Complex']]],
+  ['operator_2d_2',['operator-',['../class_complex.html#ad9363d22ed922d55de7e817eddf90ec0',1,'Complex']]],
+  ['operator_3c_3c_3',['operator&lt;&lt;',['../class_complex.html#a087b35621ae8bcdc365a7329a86c847a',1,'Complex']]],
+  ['operator_3e_3e_4',['operator&gt;&gt;',['../class_complex.html#a42f0189696ad5089d4ecdb84923ebd88',1,'Complex']]]
+];
